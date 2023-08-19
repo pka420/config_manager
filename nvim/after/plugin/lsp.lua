ec1924451ed88a16a5982b053f89c64d01fe4eb1
Piyush Khurana <pka42@sfu.ca>
2023-08-20 01:56:35 +0530

    some changes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,7 +2,6 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-
 lsp.ensure_installed({
   'tsserver',
   'clangd',
@@ -34,7 +33,7 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
+    suggest_lsp_servers = true,
     sign_icons = {
         error = 'E',
         warn = 'W',
@@ -46,7 +45,7 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gf", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -55,7 +54,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<leader>h", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("v", "<leader>h", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
--- nvim/lua/pka42/init.lua
+++ nvim/lua/pka42/init.lua
@@ -1,3 +1,25 @@
 require("pka42.remap")
 require("pka42.set")
 vim.cmd[[ colorscheme tokyonight ]]
+
+function reload_config()
+    for name,_ in pairs(package.loaded) do
+        if name:match('^pka42') then
+            package.loaded[name] = nil
+        end
+    end
+
+    require('pka42.remap')
+    require("pka42.set")
+
+    -- Reload after/ directory
+    local glob = vim.fn.stdpath('config') .. '/after/**/*.lua'
+    local after_lua_filepaths = vim.fn.glob(glob, true, true)
+
+    for _, filepath in ipairs(after_lua_filepaths) do
+        dofile(filepath)
+    end
+
+    vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
+end
+vim.keymap.set('n', '<leader>r', reload_config)
